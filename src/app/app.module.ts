93548ff9a69a3736909bf9ac72accf43e50fded8
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-19 19:30:12 -0300

    feat: add component `pokemon-card`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
+    PokemonCardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass'],
+})
+export class PokemonCardComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
